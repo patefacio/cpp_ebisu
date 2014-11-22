b7b6999dba8b3b3d28fcdd3c1a5ef4d866dcbb9a
Daniel Davidson <dbdavidson@yahoo.com>
2014-11-22 14:47:14 -0600

    small change required when moving to magus schema reader temporarily filtering test tables - need to clean up in code_metrics schema

--- codegen/bin/schemas/code_metrics.dart
+++ codegen/bin/schemas/code_metrics.dart
@@ -3,13 +3,20 @@ library schema.code_metrics;
 import 'dart:async';
 import 'package:ebisu_cpp/cpp.dart';
 import 'package:ebisu_cpp/db_schema.dart';
+import 'package:magus/schema.dart';
+import 'package:magus/odbc_ini.dart';
+import 'package:magus/mysql.dart';
 import '../../lib/installation.dart';
 
 Future addItems() =>
-  readMysqlSchema('code_metrics')
-  .then((Schema s) =>
-      installation
-      ..addSchemaCodeGenerator(new SchemaCodeGenerator(s)));
+  new MysqlEngine(OdbcIni.createConnectionPool('code_metrics'))
+    .createSchemaReader()
+    .readSchema('code_metrics')
+    .then((Schema s) =>
+        installation
+        ..addSchemaCodeGenerator(
+          new SchemaCodeGenerator(s)
+          ..tableFilter = (t) => !t.name.contains(new RegExp('multi|user'))));
 
 Future main() =>
   addItems().then((_) => installation.generate());
