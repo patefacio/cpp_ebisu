653142d5eedc2b7c905ce8b160668b4527f3637f
Daniel Davidson <dbdavidson@yahoo.com>
2014-12-09 17:09:10 -0600

    added timestamp header to list

--- codegen/bin/libs.dart
+++ codegen/bin/libs.dart
@@ -4,11 +4,13 @@ import '../lib/installation.dart';
 import 'libs/utils.dart' as utils;
 import 'libs/raii.dart' as raii;
 import 'libs/orm.dart' as orm;
+import 'libs/timestamp.dart' as timestamp;
 
 addItems() {
   utils.addItems();
   raii.addItems();
   orm.addItems();
+  timestamp.addItems();
 }
 
 main() {
