0cc63086618ebdc3e3dfd7ae4e4afe21a30eaf56
Daniel Davidson <dbdavidson@yahoo.com>
2015-02-11 09:49:34 -0600

    now cmake.gen.sh protect blocks are protected and may have exports

--- cpp/cmake.gen.sh
+++ cpp/cmake.gen.sh
@@ -1,7 +1,7 @@
-# custom <additional exports/flags>
+# custom <additional exports>
 export CC=$HOME/install/llvm31/bin/clang
 export CXX=$HOME/install/llvm31/bin/clang++
-# end <additional exports/flags>
+# end <additional exports>
 
 cmake -DCMAKE_BUILD_TYPE=Release -B../cmake_build/release -H.
 cmake -DCMAKE_BUILD_TYPE=Debug -B../cmake_build/debug -H.
