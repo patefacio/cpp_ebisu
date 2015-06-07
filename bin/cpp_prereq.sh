22f7e5b3c298930debf37069859c3cfbc9750089
Daniel Davidson <dbdavidson@yahoo.com>
2015-06-07 18:28:52 -0500

    updating for travis

---
+++ bin/cpp_prereq.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/philsquared/Catch install/catch
+git clone https://github.com/gabime/spdlog install/spdlog
--- cpp/.cmake.gen.sh
+++ cpp/.cmake.gen.sh
@@ -7,3 +7,4 @@ export CXX=clang++
 
 cmake -DCMAKE_BUILD_TYPE=Release -B../cmake_build/release -H.
 cmake -DCMAKE_BUILD_TYPE=Debug -B../cmake_build/debug -H.
+
