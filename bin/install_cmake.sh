9bb4134d611a0a35925cda55e40e19d8f8bcd9a6
Daniel Davidson <dbdavidson@yahoo.com>
2015-11-21 11:55:11 -0600

    pass --no-check-certificate to wget for cmake.org

--- bin/install_cmake.sh
+++ bin/install_cmake.sh
@@ -1,4 +1,4 @@
 mkdir -p build_dir
-wget -O build_dir/cmake-3.1.0-Linux-x86_64.tar.gz http://www.cmake.org/files/v3.1/cmake-3.1.0-Linux-x86_64.tar.gz
+wget --no-check-certificate -O build_dir/cmake-3.1.0-Linux-x86_64.tar.gz http://www.cmake.org/files/v3.1/cmake-3.1.0-Linux-x86_64.tar.gz
 tar xvfz build_dir/cmake-3.1.0-Linux-x86_64.tar.gz -C build_dir
 
