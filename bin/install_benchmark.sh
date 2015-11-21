b558bac60c8c586fb11fe3f89b96b61c0b4a17e3
Daniel Davidson <dbdavidson@yahoo.com>
2015-11-21 13:56:55 -0600

    use installed cmake

--- bin/install_benchmark.sh
+++ bin/install_benchmark.sh
@@ -1,4 +1,5 @@
 mkdir -p build_dir
+MY_CMAKE=$(pwd)/build_dir/cmake-3.1.0-Linux-x86_64/bin/cmake
 git clone https://github.com/google/benchmark build_dir/benchmark
-cd build_dir/benchmark && cmake && sudo make install && cd ../..
+cd build_dir/benchmark && $MY_CMAKE && sudo make install && cd -
 
--- bin/install_hdf5.sh
+++ bin/install_hdf5.sh
@@ -5,8 +5,8 @@ tar xvfz build_dir/CMake-hdf5-1.8.16.tar.gz -C build_dir
 MY_CMAKE=$(pwd)/build_dir/cmake-3.1.0-Linux-x86_64/bin/cmake
 $MY_CMAKE build_dir/CMake-hdf5-1.8.16/hdf5-1.8.16 -Bbuild_dir/CMake-hdf5-1.8.16_build
 tar xvfz build_dir/CMake-hdf5-1.8.16/SZip.tar.gz -C build_dir
-cd build_dir/SZip && $MY_CMAKE . && sudo make install && cd ../..
+cd build_dir/SZip && $MY_CMAKE . && sudo make install && cd -
 tar xvfz build_dir/CMake-hdf5-1.8.16/ZLib.tar.gz -C build_dir
-cd build_dir/ZLib && $MY_CMAKE . && sudo make install && cd ../..
-cd build_dir/CMake-hdf5-1.8.16_build && sudo make install && cd ../..
+cd build_dir/ZLib && $MY_CMAKE . && sudo make install && cd -
+cd build_dir/CMake-hdf5-1.8.16_build && sudo make install && cd -
 
