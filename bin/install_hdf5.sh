f2792ac98d2b1af2cc2e451b3fe364fc71e37357
Daniel Davidson <dbdavidson@yahoo.com>
2015-11-21 13:20:08 -0600

    build szip and zip for hdf5

--- bin/install_hdf5.sh
+++ bin/install_hdf5.sh
@@ -1,6 +1,12 @@
+#!/bin/sh
 mkdir -p build_dir
 wget -O build_dir/CMake-hdf5-1.8.16.tar.gz http://www.hdfgroup.org/ftp/HDF5/current/src/CMake-hdf5-1.8.16.tar.gz
 tar xvfz build_dir/CMake-hdf5-1.8.16.tar.gz -C build_dir
-build_dir/cmake-3.1.0-Linux-x86_64/bin/cmake build_dir/CMake-hdf5-1.8.16/hdf5-1.8.16 -Bbuild_dir/CMake-hdf5-1.8.16_build
-cd build_dir/CMake-hdf5-1.8.16_build && sudo make install
+MY_CMAKE=$(pwd)/build_dir/cmake-3.1.0-Linux-x86_64/bin/cmake
+$MY_CMAKE build_dir/CMake-hdf5-1.8.16/hdf5-1.8.16 -Bbuild_dir/CMake-hdf5-1.8.16_build
+tar xvfz build_dir/CMake-hdf5-1.8.16/SZip.tar.gz -C build_dir
+cd build_dir/SZip && $MY_CMAKE . && sudo make install && cd ../..
+tar xvfz build_dir/CMake-hdf5-1.8.16/ZLib.tar.gz -C build_dir
+cd build_dir/ZLib && $MY_CMAKE . && sudo make install && cd ../..
+cd build_dir/CMake-hdf5-1.8.16_build && sudo make install && cd ../..
 
