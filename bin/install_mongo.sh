4a4dfcaf1a39a85bb6b614ad9df0b2036b239389
Daniel Davidson <dbdavidson@yahoo.com>
2015-11-23 12:56:14 -0600

    fix mongo source path

--- bin/install_mongo.sh
+++ bin/install_mongo.sh
@@ -1,5 +1,5 @@
 mkdir -p build_dir
 wget -O build_dir/mongodb-linux-x86_64-2.4.14.tgz http://downloads.mongodb.org/cxx-driver/mongodb-linux-x86_64-2.4.14.tgz?_ga=1.219143706.2012223159.1446993713
 tar xvzf build_dir/mongodb-linux-x86_64-2.4.14.tgz -C build_dir
-cd build_dir/mongodb-linux-x86_64-2.4.14 && scons && sudo scons install && cd -
+cd build_dir/mongo-cxx-driver-v2.4 && scons && sudo scons install && cd -
 
