4f2c3eefb8536622ca0a14ca40e0211c287b9896
Daniel Davidson <dbdavidson@yahoo.com>
2015-12-20 17:45:49 -0600

    added environment.cpp with main

--- cpp/tests/environment/header.environment.cpp
+++ cpp/tests/environment/header.environment.cpp
@@ -17,7 +17,7 @@ SCENARIO("read environment") {
   setenv("PATH", "/foo:/goo", 1);
   auto& env = Environment::instance();
   auto& env_map = env.environment();
-  auto &path_variable_map = env.path_variable_map();
+  auto& path_variable_map = env.path_variable_map();
 
   REQUIRE(env_map.find("HOME")->second == getenv("HOME"));
   Environment::Path_list_t paths = path_variable_map.find("PATH")->second;
