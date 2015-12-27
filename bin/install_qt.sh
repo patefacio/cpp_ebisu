bbfaf38169190151b22e336826ed27d7cbeb38fc
Daniel Davidson <dbdavidson@yahoo.com>
2015-12-27 09:26:05 -0600

    troubleshooting travis build/qt usage

--- bin/install_qt.sh
+++ bin/install_qt.sh
@@ -2,3 +2,5 @@ SCRIPT=$(readlink -f "$0")
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
 sudo apt-get install qtdeclarative5-dev
+echo should have qt at!!!
+find /usr/include/qt5
