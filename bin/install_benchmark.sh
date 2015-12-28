86ec20cf533f4ce76a429b6e995692c631d322db
Daniel Davidson <dbdavidson@yahoo.com>
2015-12-28 16:20:54 -0600

    update to later cmake for benchmark and hdf5

--- bin/install_benchmark.sh
+++ bin/install_benchmark.sh
@@ -1,7 +1,7 @@
 SCRIPT=$(readlink -f "$0")
 BUILD_DIR=$(dirname $(dirname "$SCRIPT"))/build_dir
 mkdir -p $BUILD_DIR
-MY_CMAKE=$BUILD_DIR/cmake-3.1.0-Linux-x86_64/bin/cmake
+MY_CMAKE=$BUILD_DIR/cmake-3.4.0-Linux-x86_64/bin/cmake
 git clone https://github.com/google/benchmark $BUILD_DIR/benchmark
 cd $BUILD_DIR/benchmark && $MY_CMAKE . && sudo make install && cd -
 
--- bin/install_hdf5.sh
+++ bin/install_hdf5.sh
@@ -3,7 +3,7 @@ BUILD_DIR=$(dirname $(dirname "$SCRIPT"))/build_dir
 mkdir -p $BUILD_DIR
 wget -O $BUILD_DIR/CMake-hdf5-1.8.16.tar.gz http://www.hdfgroup.org/ftp/HDF5/current/src/CMake-hdf5-1.8.16.tar.gz
 tar xvfz $BUILD_DIR/CMake-hdf5-1.8.16.tar.gz -C $BUILD_DIR
-MY_CMAKE=$BUILD_DIR/cmake-3.1.0-Linux-x86_64/bin/cmake
+MY_CMAKE=$BUILD_DIR/cmake-3.4.0-Linux-x86_64/bin/cmake
 $MY_CMAKE $BUILD_DIR/CMake-hdf5-1.8.16/hdf5-1.8.16 -B$BUILD_DIR/CMake-hdf5-1.8.16_build
 tar xvfz $BUILD_DIR/CMake-hdf5-1.8.16/SZip.tar.gz -C $BUILD_DIR
 cd $BUILD_DIR/SZip && $MY_CMAKE . && sudo make install && cd -
