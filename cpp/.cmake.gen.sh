49b9290322ecd0c5393dfbefe7ae5f72faa9571f
Daniel Davidson <dbdavidson@yahoo.com>
2015-12-28 22:59:36 -0600

    trying to get to installed cmake

--- cpp/.cmake.gen.sh
+++ cpp/.cmake.gen.sh
@@ -9,5 +9,5 @@ export CXX=g++-4.9
 
 # end <additional exports>
 
-cmake -DCMAKE_PREFIX_PATH=/usr/include/qt5 -DCMAKE_BUILD_TYPE=Release -B../cmake_build/release -H.
-cmake -DCMAKE_PREFIX_PATH=/usr/include/qt5 -DCMAKE_BUILD_TYPE=Debug -B../cmake_build/debug -H.
+$CMAKE -DCMAKE_PREFIX_PATH=/usr/include/qt5 -DCMAKE_BUILD_TYPE=Release -B../cmake_build/release -H.
+$CMAKE -DCMAKE_PREFIX_PATH=/usr/include/qt5 -DCMAKE_BUILD_TYPE=Debug -B../cmake_build/debug -H.
