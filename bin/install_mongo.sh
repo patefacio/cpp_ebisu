027ec51b1b497166da5fa372eed5e0dcb3e7f579
Daniel Davidson <dbdavidson@yahoo.com>
2016-02-25 06:44:04 -0600

    trying to build mongo in travis

--- bin/install_mongo.sh
+++ bin/install_mongo.sh
@@ -2,6 +2,8 @@ SCRIPT=$(readlink -f "$0")
 BUILD_DIR=$(dirname $(dirname "$SCRIPT"))/build_dir
 mkdir -p $BUILD_DIR
 MY_CMAKE=$BUILD_DIR/cmake-3.4.0-Linux-x86_64/bin/cmake
+git clone -b master https://github.com/mongodb/mongo-c-driver.git $BUILD_DIR/mongo-c-driver
 git clone https://github.com/mongodb/mongo-cxx-driver.git $BUILD_DIR/mongo-cxx-driver
-cd $BUILD_DIR/mongo-cxx-driver && $MY_CMAKE . && sudo make install && cd -
+pushd $BUILD_DIR/mongo-c-driver; ./autogen.sh --enable-tests=no --enable-examples=no --with-libbson=bundled; make; sudo make install; popd;
+pushd $BUILD_DIR/mongo-cxx-driver && $MY_CMAKE . && sudo make install && cd -; popd
 
