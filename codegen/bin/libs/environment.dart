a382ea04a5ccc07af1f5e369424a795f4aa219bb
Daniel Davidson <dbdavidson@yahoo.com>
2016-03-08 21:19:53 -0600

    trying new approach to specifying types that invokes the namer

--- codegen/bin/libs/environment.dart
+++ codegen/bin/libs/environment.dart
@@ -4,44 +4,48 @@ import 'package:ebisu_cpp/ebisu_cpp.dart';
 import '../../lib/ebisu_installation.dart';
 
 final environment = lib('environment')
-  ..namespace = namespace([ 'ebisu', 'environment' ])
+  ..namespace = namespace(['ebisu', 'environment'])
   ..headers = [
     header('environment')
-    ..classes = [
-      class_('environment')
-      ..includes.addAll([
-        'ebisu/utils/streamers/vector.hpp',
-        'ebisu/utils/streamers/map.hpp',
-        'boost/filesystem.hpp',
-        'boost/regex.hpp',
-        'unistd.h',
-        'sstream',
-        'vector',
-        'exception',
-      ])
-      ..defaultCtor.customCodeBlock.snippets.add('read_environment();')
-      ..customBlocks = [ clsPrivate ]
-      ..doc = '''
+      ..classes = [
+        class_('environment')
+          ..includes.addAll([
+            'ebisu/utils/streamers/vector.hpp',
+            'ebisu/utils/streamers/map.hpp',
+            'boost/filesystem.hpp',
+            'boost/regex.hpp',
+            'unistd.h',
+            'sstream',
+            'vector',
+            'exception',
+          ])
+          ..defaultCtor.customCodeBlock.snippets.add('read_environment();')
+          ..customBlocks = [clsPrivate]
+          ..doc = '''
 Singleton that when initialized churns through various environment variables
 like LD_LIBRARY_PATH, MANPATH, PATH, etc...
 The purpose is to make the paths those variables refer to available to code.'''
-      ..usings = [
-        using('environment_map', 'std::map< std::string, std::string >'),
-        using('path_variable_set', 'std::set< std::string >'),
-        using('path', 'boost::filesystem::path'),
-        using('path_list', 'std::vector< Path_t >'),
-        using('path_variable_map', 'std::map< std::string, Path_list_t >'),
+          ..usings = [
+            using('environment_map', 'std::map< std::string, std::string >'),
+            using('path_variable_set', 'std::set< std::string >'),
+            using('path', 'boost::filesystem::path'),
+            using('path_list', 'std::vector< Path_t >'),
+            using('path_variable_map', 'std::map< std::string, Path_list_t >'),
+          ]
+          ..isSingleton = true
+          ..usesStreamers = true
+          ..members = [
+            member('environment')
+              ..type = #u.environment_map
+              ..isByRef = true
+              ..access = ro,
+            member('path_variable_map')
+              ..type = #u.path_variable_map
+              ..isByRef = true
+              ..access = ro,
+          ],
       ]
-      ..isSingleton = true
-      ..usesStreamers = true
-      ..members = [
-        member('environment')..type = 'Environment_map_t'..isByRef = true..access = ro,
-        member('path_variable_map')..type = 'Path_variable_map_t'..isByRef = true..access = ro,
-      ],
-    ]
-    ..testScenarios = [
-      testScenario('read environment'),
-    ],
+      ..testScenarios = [testScenario('read environment'),],
   ];
 
 addItems() => ebisuInstallation.addLibs([environment]);
--- codegen/bin/libs/linux_specific.dart
+++ codegen/bin/libs/linux_specific.dart
@@ -28,49 +28,42 @@ get processorEntries {
   final byLineRe = new RegExp(r'\n');
 
   return new File('/proc/cpuinfo')
-  .readAsStringSync()
-  .split(procRe)
-  .skip(1)  // skip first empty element post-split
-  .take(1)  // take first processor entry
-  .map((String proc) =>
-      proc
-      .split(byLineRe)
-      .skip(1)
-      .where((String line) => line.length > 0)
-      .map((String line) =>
-        line
-          .split(r':')
-          .take(1)
-          .map((String word) =>
-              word
-              .trim()
-              .toLowerCase()
-              .replaceAll(' ', '_'))
-          .first)
-      .toList())
-  .first;
+      .readAsStringSync()
+      .split(procRe)
+      .skip(1) // skip first empty element post-split
+      .take(1) // take first processor entry
+      .map((String proc) => proc
+          .split(byLineRe)
+          .skip(1)
+          .where((String line) => line.length > 0)
+          .map((String line) => line
+              .split(r':')
+              .take(1)
+              .map((String word) =>
+                  word.trim().toLowerCase().replaceAll(' ', '_'))
+              .first)
+          .toList())
+      .first;
 }
 
 final linux_specific = lib('linux_specific')
   ..requiresLogging = true
-  ..namespace = namespace([ 'ebisu', 'linux_specific' ])
+  ..namespace = namespace(['ebisu', 'linux_specific'])
   ..headers = [
     header('application_signal_handler')
-    ..doc = 'Support for catching signals and associating handlers'
-    ..includes = [ 'csignal', 'mutex' ]
-    ..doc = 'Support for consistent catching/handling of signals'
-    ..testScenarios = [
-      testScenario('catching signal', [
-        given('registered handler', [
-          when('signal sent', [
-            then('signal handled')
+      ..doc = 'Support for catching signals and associating handlers'
+      ..includes = ['csignal', 'mutex']
+      ..doc = 'Support for consistent catching/handling of signals'
+      ..testScenarios = [
+        testScenario('catching signal', [
+          given('registered handler', [
+            when('signal sent', [then('signal handled')])
           ])
         ])
-      ])
-    ]
-    ..classes = [
-      class_('application_signal_handler')
-      ..doc = '''
+      ]
+      ..classes = [
+        class_('application_signal_handler')
+          ..doc = '''
 A singleton class to allow registration of common interrupting signal handlers.
 
   - SIGHUP
@@ -97,176 +90,190 @@ with *main*. Register a handler to deal with the signal. Prior to
 exiting the *main* call
 [Application_signal_handler::stop_handler_thread].
 '''
-      ..customBlocks = [ clsPublic, clsPrivate ]
-      ..template = [
-        'typename LOCK_TYPE = std::mutex',
-        'typename GUARD_TYPE = std::lock_guard< LOCK_TYPE >',
-      ]
-      ..defaultCtor.includesProtectBlock = true
-      ..dtor.includesProtectBlock = true
-      ..usings = [
-        using('thread', 'std::thread'),
-        usingUptr('thread', 'Thread_t'),
-        using('lock_type', 'LOCK_TYPE'),
-        using('guard_type', 'GUARD_TYPE'),
-        using('handler_function', 'std::function< bool(int) >'),
-        using('handler_function_list', 'std::vector< Handler_function_t >'),
-      ]
-      ..isSingleton = true
-      ..members = [
-        member('handler_function_list')
-        ..doc = 'User defined handler functions registered with [add_handler]'
-        ..type='Handler_function_list_t',
-        member('instance_lock')
-        ..doc = 'Lock protecting access to [handler_function_list_t]'
-        ..type='Lock_type_t',
-        member('handler_thread')
-        ..doc = 'Thread that calls sigwait to process signals'
-        ..type = 'Thread_uptr_t',
-        member('exit_requested')
-        ..doc = 'Used to cleanly exit via signal'
-        ..init = false,
-      ],
-
-      class_('application_signal_handler_exit')
-      ..doc = '''
+          ..customBlocks = [clsPublic, clsPrivate]
+          ..template = [
+            'typename LOCK_TYPE = std::mutex',
+            'typename GUARD_TYPE = std::lock_guard< LOCK_TYPE >',
+          ]
+          ..defaultCtor.includesProtectBlock = true
+          ..dtor.includesProtectBlock = true
+          ..usings = [
+            using('thread', 'std::thread'),
+            usingUptr('thread', 'Thread_t'),
+            using('lock_type', 'LOCK_TYPE'),
+            using('guard_type', 'GUARD_TYPE'),
+            using('handler_function', 'std::function< bool(int) >'),
+            using('handler_function_list', 'std::vector< Handler_function_t >'),
+          ]
+          ..isSingleton = true
+          ..members = [
+            member('handler_function_list')
+              ..doc =
+                  'User defined handler functions registered with [add_handler]'
+              ..type = #u.handler_function_list,
+            member('instance_lock')
+              ..doc = 'Lock protecting access to [handler_function_list_t]'
+              ..type = #u.lock_type,
+            member('handler_thread')
+              ..doc = 'Thread that calls sigwait to process signals'
+              ..type = #u.thread_uptr,
+            member('exit_requested')
+              ..doc = 'Used to cleanly exit via signal'
+              ..init = false,
+          ],
+        class_('application_signal_handler_exit')
+          ..doc = '''
 Class to clean up application handler thread.
 
 Use an automatic variable and RAII in main to ensure cleanup.
 '''
-      ..dtor.includesProtectBlock = true
-      ..defaultCtor.usesDefault = true
-      ..isNoncopyable = true,
-
-    ],
-
+          ..dtor.includesProtectBlock = true
+          ..defaultCtor.usesDefault = true
+          ..isNoncopyable = true,
+      ],
     header('umask_scoped_set')
-    ..includes = [
-      'sys/types.h',
-      'sys/stat.h',
-    ]
-    ..testScenarios = [
-      testScenario('unit_test_umask_scoped_set',
-          given('current umask is something',
-              when('setting umask to something else',
-                  then('it is set and reverted on exit')))),
-    ]
-    ..classes = [
-      class_('umask_scoped_set')
-      ..descr = '''
+      ..includes = ['sys/types.h', 'sys/stat.h',]
+      ..testScenarios = [
+        testScenario(
+            'unit_test_umask_scoped_set',
+            given(
+                'current umask is something',
+                when('setting umask to something else',
+                    then('it is set and reverted on exit')))),
+      ]
+      ..classes = [
+        class_('umask_scoped_set')
+          ..descr = '''
 A typical use of this is to set the umask for a process to provide
 settings of system resources (files, mutexes, mappings,...)  to allow
 group members in. For instance, an app may create shared memory, and
 therefore an entry in /dev/shm, but to share it the permissions must
 work. umask settings play a role.'''
-      ..memberCtors = [
-        memberCtor([
-          memberCtorParm('previous_mode')
-          ..parmDecl = 'mode_t new_mode'
-          ..init = 'umask(new_mode)'
-        ])
-        ..doc = 'Construct Umask_scoped_set with specified *new_mode*'
-        ..isExplicit = true
-      ]
-      ..customBlocks = [ clsPublic ]
-      ..members = [
-        member('previous_mode')
-        ..descr = 'Original mode, restored to on destruction'
-        ..type = 'mode_t'..access = ro..hasNoInit = true
-      ]
-    ],
+          ..memberCtors = [
+            memberCtor([
+              memberCtorParm('previous_mode')
+                ..parmDecl = 'mode_t new_mode'
+                ..init = 'umask(new_mode)'
+            ])
+              ..doc = 'Construct Umask_scoped_set with specified *new_mode*'
+              ..isExplicit = true
+          ]
+          ..customBlocks = [clsPublic]
+          ..members = [
+            member('previous_mode')
+              ..descr = 'Original mode, restored to on destruction'
+              ..type = 'mode_t'
+              ..access = ro
+              ..hasNoInit = true
+          ]
+      ],
     header('linux_exceptions')
-    ..includes = [
-      'boost/exception/exception.hpp',
-      'boost/exception/error_info.hpp',
-      'exception',
-      'string',
-    ]
-    ..classes = [
-      class_('system_file_parse_exception')
-      ..doc = 'Indicates parsing of system file failed'
-      ..usings = ['filename_tag = boost::error_info<struct tag_filename_tag, std::string>']
-      ..bases = [
-        base('boost::exception')..isVirtual = true..cppAccess = public,
-        base('std::exception')..isVirtual = true..cppAccess = public,
+      ..includes = [
+        'boost/exception/exception.hpp',
+        'boost/exception/error_info.hpp',
+        'exception',
+        'string',
       ]
-    ],
+      ..classes = [
+        class_('system_file_parse_exception')
+          ..doc = 'Indicates parsing of system file failed'
+          ..usings = [
+            'filename_tag = boost::error_info<struct tag_filename_tag, std::string>'
+          ]
+          ..bases = [
+            base('boost::exception')
+              ..isVirtual = true
+              ..cppAccess = public,
+            base('std::exception')
+              ..isVirtual = true
+              ..cppAccess = public,
+          ]
+      ],
     header('cpu_info')
-    ..withTest((Test test) {
-      test.includes.addAll(['iostream', 'sstream']);
-    })
-    ..includes = [
-      'ebisu/linux_specific/linux_exceptions.hpp',
-      'ebisu/utils/streamers/map.hpp',
-      'ebisu/utils/streamers/vector.hpp',
-      'ebisu/utils/exception/make_exception.hpp',
-      'fstream', 'iterator', 'boost/regex.hpp', 'boost/thread.hpp',
-      'boost/lexical_cast.hpp',
-      'sys/types.h', 'unistd.h', 'boost/algorithm/string.hpp',
-    ]
-    ..classes = [
-      class_('processor')
-      ..isStreamable = true
-      ..usesStreamers = true
-      ..usingsPostDecl = [ using('Processor_list', 'std::vector< Processor >') ]
-      ..memberCtors = [ memberCtor(['processor', 'proc_map']) ]
-      ..withCustomBlock(clsPublic, (CodeBlock cb) {
-        cb.snippets
-          .add(
-              br(processorEntries
-                  .map((String entry) {
-                    return '''
+      ..withTest((Test test) {
+        test.includes.addAll(['iostream', 'sstream']);
+      })
+      ..includes = [
+        'ebisu/linux_specific/linux_exceptions.hpp',
+        'ebisu/utils/streamers/map.hpp',
+        'ebisu/utils/streamers/vector.hpp',
+        'ebisu/utils/exception/make_exception.hpp',
+        'fstream',
+        'iterator',
+        'boost/regex.hpp',
+        'boost/thread.hpp',
+        'boost/lexical_cast.hpp',
+        'sys/types.h',
+        'unistd.h',
+        'boost/algorithm/string.hpp',
+      ]
+      ..classes = [
+        class_('processor')
+          ..isStreamable = true
+          ..usesStreamers = true
+          ..usingsPostDecl = [
+            using('Processor_list', 'std::vector< Processor >')
+          ]
+          ..memberCtors = [
+            memberCtor(['processor', 'proc_map'])
+          ]
+          ..withCustomBlock(clsPublic, (CodeBlock cb) {
+            cb.snippets.add(br(processorEntries.map((String entry) {
+              return '''
 std::string $entry() const {
   auto found = proc_map_.find("$entry");
   return (found != proc_map_.end())? found->second : "";
 }
 ''';
-                  })));
-      })
-      ..descr = '''
+            })));
+          })
+          ..descr = '''
 Information stored on a processor basis in cpuinfo.
 The fields in this class are dynamically generated from parsing
 cpuinfo.'''
-      ..usings = [ using('proc_map', 'std::map< std::string, std::string >') ]
-      ..members = [
-        member('proc_map')
-        ..doc = 'Map of processors'
-        ..type = 'Proc_map_t'..isByRef = true..access = ro,
-        member('processor')
-        ..type = 'int'..access = ro
-      ],
-      class_('cpu_info')
-      ..descr = '''
+          ..usings = [using('proc_map', 'std::map< std::string, std::string >')]
+          ..members = [
+            member('proc_map')
+              ..doc = 'Map of processors'
+              ..type = #u.proc_map
+              ..isByRef = true
+              ..access = ro,
+            member('processor')
+              ..type = 'int'
+              ..access = ro
+          ],
+        class_('cpu_info')
+          ..descr = '''
 Class to parse cpuinfo file. This might be of use to interrogate
 from code the stats of the machine for better enabling <apple to apple>
 comparisons.
 '''
-      ..testScenarios = [
-        testScenario('cpu info')
-        ..givens = [
-          given('the single cpu_info instance',
-              when('accessing processors',
-                  [
-                    then('some processors are found'),
-                    then('the first processor has flags'),
-                    then('the fpu flag is yes'),
-                  ])
-                )
-        ]
+          ..testScenarios = [
+            testScenario('cpu info')
+              ..givens = [
+                given(
+                    'the single cpu_info instance',
+                    when('accessing processors', [
+                      then('some processors are found'),
+                      then('the first processor has flags'),
+                      then('the fpu flag is yes'),
+                    ]))
+              ]
+          ]
+          ..isStreamable = true
+          ..usesStreamers = true
+          ..isSingleton = true
+          ..withDefaultCtor(
+              (defaultCtor) => defaultCtor..includesProtectBlock = true)
+          ..customBlocks = [clsPrivate]
+          ..members = [
+            member('processors')
+              ..doc = 'List of processors'
+              ..type = #u.processor_list
+              ..isByRef = true
+              ..access = ro
+          ],
       ]
-      ..isStreamable = true
-      ..usesStreamers = true
-      ..isSingleton = true
-      ..withDefaultCtor((defaultCtor) =>
-          defaultCtor..includesProtectBlock = true)
-      ..customBlocks = [ clsPrivate ]
-      ..members = [
-        member('processors')
-        ..doc = 'List of processors'
-        ..type = 'Processor_list_t'..isByRef = true..access = ro
-      ],
-    ]
-
   ];
 
 addItems() => ebisuInstallation.addLibs([linux_specific]);
